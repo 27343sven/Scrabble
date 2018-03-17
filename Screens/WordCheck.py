09b9acef9bdaf91820170aa03fec95092e54a0fc
Facehit <Cubot1@live.nl>
2018-03-17 17:06:56 +0100

    Finished code for Wordchecker

--- Screens/WordCheck.py
+++ Screens/WordCheck.py
@@ -4,8 +4,7 @@ class WordCheck:
         file = open("woorden.lst","r")
         self.lst_woorden = file.read().splitlines()
         file.close()
-        self.lst_woorden = self.lst_woorden[5:]
-
+        self.lst_woorden = self.lst_woorden[1:]
 
     def woordChecker(self, str_woord):
         if str_woord in self.lst_woorden:
@@ -16,17 +15,35 @@ class WordCheck:
     def woordToevoegen(self, str_woord):
         if str_woord in self.lst_woorden:
             return "Dit woord staat al in de lijst."
-        elif str_woord.len() < 3:
+        elif len(str_woord) < 3:
             return "Dit woord is te klein om toe te voegen."
         else:
             self.lst_woorden.append(str_woord)
+            self.lst_woorden = sorted(self.lst_woorden, key=len)
+            file = open("woorden.lst", "w")
+            file.write(" \n\r")
+            file.close()
+            file = open("woorden.lst", "a")
+            for word in self.lst_woorden:
+                file.write(word+"\r\n")
+            file.close()
             return "OK"
 
     def woordVerwijderen(self, str_woord):
         if str_woord not in self.lst_woorden:
             return "Dit woord staat niet in de lijst."
         else:
+            self.lst_woorden.remove(str_woord)
+            self.lst_woorden = sorted(self.lst_woorden, key=len)
+            file = open("woorden.lst", "w")
+            file.write(" \n\r")
+            file.close()
+            file = open("woorden.lst", "a")
+            for word in self.lst_woorden:
+                file.write(word + "\r\n")
+            file.close()
             return "OK"
+
 
 
 
@@ -34,3 +51,5 @@ class WordCheck:
 
 if __name__ == '__main__':
     Woord = WordCheck()
+    print(Woord.woordVerwijderen("aag"))
+    print(Woord.woordToevoegen("aagg"))
